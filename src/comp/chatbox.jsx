80fb5772d0b3b5989c94f233546d2f579d4c73f5
Rani <raniking15@gmail.com>
2024-12-23 16:53:01 +0200

    Feature: Added integration to server websocket.

--- src/comp/chatbox.jsx
+++ src/comp/chatbox.jsx
@@ -1,36 +1,44 @@
-import { useState } from 'react';
+import { useState,useEffect} from 'react';
+import useWebSocket from 'react-use-websocket';
 import MessageList from './MessageList';
 import MessageInput from './MessageInput';
 
 export default function Chatbox() {
+  // TODO: change to no local address
+  const [messageHistory, setMessageHistory] = useState([])
+  const [webSocketUrl,setWebSocketUrl] = useState('ws://localhost:9910/api/chat/conversation')
+  const {sendMessage,lastMessage,readyState} = useWebSocket(webSocketUrl)
 
-  const [messages, setMessages] = useState([
-    { id: 1, type: 'incoming', text: 'Hello! How can I help you?' },
-    { id: 2, type: 'outgoing', text: 'hii' },
-  ]);
+  // Recieve message and store it in messageHistory
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const m = {
+        id: Date.now(),
+        type:'incoming',
+        text:lastMessage.data
+      }
+      setMessageHistory((prev) => prev.concat(m));
+    }
+  }, [lastMessage]);
 
-  const sendMessage = (text) => {
+  const handleSendMessage = (msg) =>{
+    sendMessage(msg)
+    const m = {
+      id: Date.now(),
+      type:'outgoing',
+      text:msg
+    }
+    setMessageHistory((prev) => prev.concat(m))
+  }
 
-    const newMessage = { id: Date.now(), type: 'outgoing', text };
-    setMessages((prev) => [...prev, newMessage]);
-
-    setTimeout(() => {
-      const aiResponse = {
-        id: Date.now() + 1,
-        type: 'incoming',
-        text: `You said: "${text}". Let me help!`,
-      };
-      setMessages((prev) => [...prev, aiResponse]);
-    }, 1000);
-  };
-
+  
   return (
     <div className="flex flex-col w-full h-screen bg-white shadow-lg overflow-hidden">
       <div className="bg-gray-800 text-white text-center p-4 font-bold">
         Chat Assistant
       </div>
-      <MessageList messages={messages} />
-      <MessageInput onSend={sendMessage} />
+      <MessageList messages={messageHistory} />
+      <MessageInput onSend={handleSendMessage} />
     </div>
   );
 }
