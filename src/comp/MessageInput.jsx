e9b86a97fe53ebee477e16fd8577d62aa1b34fda
Rani Giro <raniking15@gmail.com>
2024-12-25 19:36:15 +0200

    Refactor + enhancement: Change message protocol on websocket to json over websocket, added on enter press send.

--- src/comp/MessageInput.jsx
+++ src/comp/MessageInput.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { ArrowForwardOutlined} from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { ArrowForwardOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 export default function MessageInput({ onSend }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   const handleSend = () => {
     if (text.trim()) {
       onSend(text);
-      setText('');
+      setText("");
+    }
+  };
+
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      handleSend();
     }
   };
   return (
@@ -21,12 +26,12 @@ export default function MessageInput({ onSend }) {
         value={text}
         onChange={(e) => setText(e.target.value)}
         className="flex-grow p-2 border border-gray-300 rounded-full outline-none focus:ring-2 focus:ring-blue-500 mr-8"
+        onKeyDown={handleEnter}
       />
 
-      <IconButton className='ml-8' onClick={handleSend}>
-        <ArrowForwardOutlined/>
+      <IconButton className="ml-8" onClick={handleSend}>
+        <ArrowForwardOutlined />
       </IconButton>
-
     </div>
   );
 }
--- src/comp/chatbox.jsx
+++ src/comp/chatbox.jsx
@@ -1,36 +1,45 @@
-import { useState,useEffect} from 'react';
-import useWebSocket from 'react-use-websocket';
-import MessageList from './MessageList';
-import MessageInput from './MessageInput';
+import { useState, useEffect } from "react";
+import useWebSocket from "react-use-websocket";
+import MessageList from "./MessageList";
+import MessageInput from "./MessageInput";
 
 export default function Chatbox() {
-  const [messageHistory, setMessageHistory] = useState([])
-  const [webSocketUrl,setWebSocketUrl] = useState("wss://trip-me-up-server.onrender.com/api/chat/conversation")
-  const {sendMessage,lastMessage,readyState} = useWebSocket(webSocketUrl)
+  const [messageHistory, setMessageHistory] = useState([]);
+  const [webSocketUrl, setWebSocketUrl] = useState(
+    "wss://trip-me-up-server.onrender.com/api/chat/conversation",
+  );
+  const { sendMessage, lastMessage, readyState } = useWebSocket(webSocketUrl);
 
   // Recieve message and store it in messageHistory
   useEffect(() => {
     if (lastMessage !== null) {
+      const msg = JSON.parse(lastMessage.data);
+
       const m = {
         id: Date.now(),
-        type:'incoming',
-        text:lastMessage.data
+        type: "incoming",
+        text: msg.message,
+        locations: null,
+      };
+      if (msg.route !== null) {
+        m.text = "Redirecting you to route...";
+        m.locations = JSON.parse(msg.route).places;
+        console.log(m.locations);
       }
       setMessageHistory((prev) => prev.concat(m));
     }
   }, [lastMessage]);
 
-  const handleSendMessage = (msg) =>{
-    sendMessage(msg)
+  const handleSendMessage = (msg) => {
+    sendMessage(msg);
     const m = {
       id: Date.now(),
-      type:'outgoing',
-      text:msg
-    }
-    setMessageHistory((prev) => prev.concat(m))
-  }
+      type: "outgoing",
+      text: msg,
+    };
+    setMessageHistory((prev) => prev.concat(m));
+  };
 
-  
   return (
     <div className="flex flex-col w-full h-screen bg-white shadow-lg overflow-hidden">
       <div className="bg-gray-800 text-white text-center p-4 font-bold">
@@ -41,4 +50,3 @@ export default function Chatbox() {
     </div>
   );
 }
-
