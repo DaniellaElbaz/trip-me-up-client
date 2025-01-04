c8622e6659d1420ff823252c5bffeb3644c4417c
Daniella <daniellaelbaz711@gmail.com>
2025-01-04 15:46:31 +0200

    fix image in the sub scrolling

--- src/comp/RouteMapSection.jsx
+++ src/comp/RouteMapSection.jsx
@@ -4,17 +4,17 @@ import { Box } from "@mui/material";
 export default function RouteMapSection({ startLocation, endLocation, stops, bottomHeight }) {
   return (
     <Box
-      className="relative"
-      style={{
-        height: `${100 - bottomHeight}%`,
-        overflow: "hidden",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        zIndex: 1,
-      }}
-    >
+  className="relative"
+  style={{
+    height: `${100 - bottomHeight}%`,
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    backgroundColor: "transparent", // רקע שקוף
+    zIndex: 1,
+  }}
+>
       <RouteMapView startLocation={startLocation} endLocation={endLocation} stops={stops} />
     </Box>
   );
