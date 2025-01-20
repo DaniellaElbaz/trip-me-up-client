e7d405589a8e47cc24f830df6898c41aaaf0ec08
Daniella <daniellaelbaz711@gmail.com>
2025-01-20 15:34:59 +0200

    delete icon

--- src/comp/CustomCard.jsx
+++ src/comp/CustomCard.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography, IconButton } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
-export default function CustomCard({ image, title, description }) {
+export default function CustomCard({ image, title, description, onDelete }) {
   return (
-    <Card sx={{ maxWidth: "100%",width:"60vh",height:"50vh", margin: "1rem", boxShadow: 3, display: "flex",
+    <Card
+      sx={{
+        maxWidth: "100%",
+        width: "60vh",
+        height: "50vh",
+        margin: "1rem",
+        boxShadow: 3,
+        display: "flex",
         flexDirection: "column",
-        alignItems: "center", }}>
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
       <CardMedia
         component="img"
         height="140"
         image={image}
         alt={title}
         sx={{
-            width: "100%",
-            height: "40vh",
-            objectFit: "cover",
-          }}
+          width: "100%",
+          height: "40vh",
+          objectFit: "cover",
+        }}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
@@ -26,6 +37,22 @@ export default function CustomCard({ image, title, description }) {
           {description}
         </Typography>
       </CardContent>
+      <IconButton
+        onClick={onDelete}
+        sx={{
+          position: "absolute",
+          bottom: "10px",
+          right: "10px",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          transition: "color 0.3s ease, background-color 0.3s ease",
+          "&:hover": {
+            backgroundColor: "rgba(255, 0, 0, 0.8)",
+            color: "white",
+          }
+        }}
+      >
+        <Delete />
+      </IconButton>
     </Card>
   );
 }
@@ -34,4 +61,5 @@ CustomCard.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  onDelete: PropTypes.func,
 };
