6ba991006b93795bf6a12df01a17ad9b150386c9
Daniella <daniellaelbaz711@gmail.com>
2025-01-24 02:01:36 +0200

    moving photo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Chat from './pages/chat.jsx'
 import Chatbox from './comp/chatbox.jsx'
 import RouteView from './pages/routeview.jsx'
 import RouteViewPoc from './pages/routeview_poc.jsx'
+import HomePage from './pages/homepage.jsx'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -10,7 +11,8 @@ function App() {
     <>
       <Router>
         <Routes>
-         <Route path="/" element={<Chatbox/>} />
+        <Route path="/" element={<HomePage/>} />
+         <Route path="/chat" element={<Chatbox/>} />
          <Route path="/routeview" element={<RouteView/> } />
           <Route path="/routeview_poc" element={<RouteViewPoc/>} />
         </Routes>
