06527cfd77eab849c101f2cd9b206d2c2a8bbae9
Daniella <daniellaelbaz711@gmail.com>
2025-01-27 14:21:53 +0200

    fix menu

--- src/comp/UserMenu.jsx
+++ src/comp/UserMenu.jsx
@@ -15,7 +15,10 @@ const UserMenu = () => {
   };
 
   return (
-    <div className="relative">
+    <div
+  className="relative"
+  style={{ zIndex: 100, position: 'relative' }}
+>
       {/* Header */}
       <Header toggleMenu={toggleMenu} />
 
@@ -24,16 +27,18 @@ const UserMenu = () => {
         <>
           {/* Overlay */}
           <div
-            className="fixed inset-0 bg-black bg-opacity-25 z-40"
-            onClick={closeMenu}
-          ></div>
+        className="fixed inset-0 bg-black bg-opacity-50"
+        style={{ zIndex: 99 }}
+        onClick={closeMenu}
+      ></div>
 
           {/* Sidebar */}
           <div
-            className={`fixed top-0 left-0 h-screen w-64 bg-white text-gray-800 shadow-lg transform ${
-              isOpen ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-300 z-50`}
-          >
+        className={`fixed top-0 left-0 h-screen w-64 bg-white text-gray-800 shadow-lg transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300`}
+        style={{ zIndex: 100 }}
+      >
             {/* Close Icon */}
             <button
               onClick={closeMenu}
--- src/pages/routeview_poc.jsx
+++ src/pages/routeview_poc.jsx
@@ -181,7 +181,7 @@ export default function RouteViewPoc() {
   }
   
   return (
-    <div className="w-screen h-screen flex flex-col overflow-hidden">
+    <div className="w-screen h-screen flex flex-col overflow-visible">
       <RouteMapSection
         startLocation={startLocation}
         endLocation={endLocation}
