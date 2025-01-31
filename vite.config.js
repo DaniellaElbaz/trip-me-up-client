133f481c501a2c8546e3d8e656a983ab2c11a722
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2025-01-31 17:27:46 +0200

    updated port binding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: { https: true },
+  server: { https: true, port: 5173 },
   plugins: [react(), mkcert()],
 })
