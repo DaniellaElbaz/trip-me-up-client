59290c13ba6cb238ecee1393cdc39596fa020be8
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2025-01-31 22:47:48 +0200

    wrong ws url
    oopsie

--- src/comp/chatbox.jsx
+++ src/comp/chatbox.jsx
@@ -11,7 +11,7 @@ export default function Chatbox() {
   const [messageHistory, setMessageHistory] = useState([]);
   const [userId, setUserId] = useState(sessionStorage.getItem("userID"));
   const [webSocketUrl, setWebSocketUrl] = useState(
-    "ws://localhost:9910/api/chat/conversation",
+    "wss://trip-me-up-server.onrender.com/api/chat/conversation",
   );
   const { sendMessage, lastMessage, readyState } = useWebSocket(webSocketUrl);
 
