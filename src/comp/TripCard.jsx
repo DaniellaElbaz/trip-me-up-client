5189103d53256eb692864f4c0c3107cbc63392e8
Daniella <daniellaelbaz711@gmail.com>
2025-02-02 23:33:08 +0200

    chang font

--- src/comp/TripCard.jsx
+++ src/comp/TripCard.jsx
@@ -7,16 +7,15 @@ import PlaceIcon from "@mui/icons-material/Place";
 function TripCard({ trip, onViewRoute }) {
   return (
     <div className="flex flex-row justify-between w-full bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105">
-      {/* תוכן בצד שמאל */}
       <div className="w-3/5 p-8 flex flex-col justify-center">
-        <h2 className="text-xl font-extrabold text-gray-900 mb-6 font-serif">Trip from {trip.places[0]} to {trip.places[trip.places.length - 1]}</h2>
+        <h2 className="text-xl font-bold text-gray-900 mb-6 ">Trip from {trip.places[0]} to {trip.places[trip.places.length - 1]}</h2>
         <p className="text-gray-600 text-xl mb-6 flex items-center font-light italic">
           <EventIcon className="mr-2" /> {trip.start_date}
         </p>
-        <p className="text-gray-800 font-semibold flex items-center text-xl font-mono">
+        <p className="text-gray-800 font-semibold flex items-center text-xl ">
           <PlaceIcon className="mr-3" /> Places:
         </p>
-        <ul className="list-disc list-inside text-gray-700 mb-8 text-xl font-sans">
+        <ul className="list-disc list-inside text-gray-700 mb-8 text-lg ">
           {trip.places.map((place, index) => (
             <li key={index}>{place}</li>
           ))}
@@ -28,7 +27,6 @@ function TripCard({ trip, onViewRoute }) {
           View Route
         </button>
       </div>
-      {/* גלריה בצד ימין */}
       <div >
         <GalleryContainer trip={trip} />
       </div>
