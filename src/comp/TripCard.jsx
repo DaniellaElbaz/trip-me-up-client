f635a44debedd3fa1e0b13773a5fee8080f78d19
Daniella <daniellaelbaz711@gmail.com>
2025-02-03 01:42:04 +0200

    responsive history page

--- src/comp/TripCard.jsx
+++ src/comp/TripCard.jsx
@@ -6,16 +6,18 @@ import PlaceIcon from "@mui/icons-material/Place";
 
 function TripCard({ trip, onViewRoute }) {
   return (
-    <div className="flex flex-row justify-between w-full bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105">
-      <div className="w-3/5 p-8 flex flex-col justify-center">
-        <h2 className="text-xl font-bold text-gray-900 mb-6 ">Trip from {trip.places[0]} to {trip.places[trip.places.length - 1]}</h2>
+    <div className="flex flex-col md:flex-row justify-between w-full bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105">
+      <div className="w-full md:w-3/5 p-8 flex flex-col justify-center">
+        <h2 className="text-xl font-bold text-gray-900 mb-6">
+          Trip from {trip.places[0]} to {trip.places[trip.places.length - 1]}
+        </h2>
         <p className="text-gray-600 text-xl mb-6 flex items-center font-light italic">
           <EventIcon className="mr-2" /> {trip.start_date}
         </p>
-        <p className="text-gray-800 font-semibold flex items-center text-xl ">
+        <p className="text-gray-800 font-semibold flex items-center text-xl">
           <PlaceIcon className="mr-3" /> Places:
         </p>
-        <ul className="list-disc list-inside text-gray-700 mb-8 text-lg ">
+        <ul className="list-disc list-inside text-gray-700 mb-8 text-lg">
           {trip.places.map((place, index) => (
             <li key={index}>{place}</li>
           ))}
@@ -27,12 +29,13 @@ function TripCard({ trip, onViewRoute }) {
           View Route
         </button>
       </div>
-      <div >
+      <div className="w-full md:w-auto">
         <GalleryContainer trip={trip} />
       </div>
     </div>
   );
 }
+
 
 TripCard.propTypes = {
   trip: PropTypes.shape({
