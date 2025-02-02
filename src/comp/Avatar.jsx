da884faddc0ba5caad2df7f9b31afa6ed9dad49d
Daniella <daniellaelbaz711@gmail.com>
2025-02-02 21:40:38 +0200

    logo href

--- src/comp/Avatar.jsx
+++ src/comp/Avatar.jsx
@@ -3,13 +3,16 @@ import React from "react";
 function Avatar() {
   return (
     <div className="flex justify-center">
-      <img
-        src="/images/logo.png"
-        alt="Trip Me Up Logo"
-        className="w-32 h-32 object-contain"
-      />
+      <a href="/">
+        <img
+          src="/images/logo.png"
+          alt="Trip Me Up Logo"
+          className="w-32 h-32 object-contain cursor-pointer"
+        />
+      </a>
     </div>
   );
 }
 
 export default Avatar;
+
