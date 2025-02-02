d1c9e1ccab034c69dba7d9b9cc21444fa51e172e
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2025-02-02 17:13:27 +0200

    Revert "revereted vite.config for dev"
    This reverts commit ab2630d8e7abddee243f43b73bc6f2e129407bf7.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: { https: true, port: 5173 },
-  plugins: [react(), mkcert()],
+  plugins: [react()],
+  base: '/'
 })
