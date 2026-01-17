9cbdd2623dafc548f0e89272abc8502e321d597a
Daniella <daniellaelbaz711@gmail.com>
2026-01-17 23:33:04 +0200

    fix local storage format

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,23 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export function useLocalStorage(key, initialValue) {
-  const [value, setValue] = useState(() => {
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.error(error);
+      console.error("Error reading localStorage key “" + key + "”: ", error);
       return initialValue;
     }
   });
 
   useEffect(() => {
     try {
-      window.localStorage.setItem(key, JSON.stringify(value));
+      window.localStorage.setItem(key, JSON.stringify(storedValue));
     } catch (error) {
-      console.error(error);
+      console.error("Error setting localStorage key “" + key + "”: ", error);
     }
-  }, [key, value]);
+  }, [key, storedValue]);
 
-  return [value, setValue];
+  return [storedValue, setStoredValue];
 }
+
+export default useLocalStorage;
