5252182e1a2cd9caa040941897493d62c34cdd59
Daniella <daniellaelbaz711@gmail.com>
2026-01-17 23:40:51 +0200

    user save update

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoritesReducer from "./favoritesSlice";
-
+import userReducer from "./userSlice";
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
+    user: userReducer,
   },
 });
